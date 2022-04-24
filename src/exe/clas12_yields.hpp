f05c1d94570361bac403c3019962de65b79da10f
Krishna Neupane <kneupane@email.sc.edu>
2022-04-23 23:52:18 -0400

    Update clas12_yields.hpp

--- src/exe/clas12_yields.hpp
+++ src/exe/clas12_yields.hpp
@@ -153,31 +153,30 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<SyncFile>& _syn
 
         output.pim_mom_mPim = event->pim_momentum();
         output.pim_theta_mPim = event->pim_theta_lab();
-        // output.pim_phi_mPim = event->pim_Phi_lab();
+        output.pim_phi_mPim = event->pim_Phi_lab();
 
         // recon mes
-        output.pim_phi_mPim = event->prot_momentum_measured();
         output.prot_mom_exclusive = event->prot_momentum_corrected();
-        output.prot_theta_exclusive = event->prot_theta_corrected();
-        output.prot_phi_exclusive = event->prot_Phi_corrected();
+        // output.prot_theta_exclusive = event->prot_theta_corrected();
+        // output.prot_phi_exclusive = event->prot_Phi_corrected();
         // output.prot_mom_exclusive = event->prot_momentum_measured();
-        // output.prot_theta_exclusive = event->prot_theta_lab_measured();
-        // output.prot_phi_exclusive = event->prot_Phi_lab_measured();
+        output.prot_theta_exclusive = event->prot_theta_lab_measured();
+        output.prot_phi_exclusive = event->prot_Phi_lab_measured();
         // output.prot_mom_corr = event->prot_momentum_corrected();
         // output.prot_theta_corr = event->prot_theta_corrected();
         // output.prot_phi_corr = event->prot_Phi_corrected();
 
-        output.pip_mom_exclusive = event->pip_momentum_measured();
-        output.pip_theta_exclusive = event->pip_momentum_corrected();
-        // output.pip_theta_exclusive = event->pip_theta_lab_measured();
+        // output.pip_mom_exclusive = event->pip_momentum_measured();
+        output.pip_mom_exclusive = event->pip_momentum_corrected();
+        output.pip_theta_exclusive = event->pip_theta_lab_measured();
         output.pip_phi_exclusive = event->pip_Phi_lab_measured();
         // output.pip_mom_corr = event->pip_momentum_corrected();
         // output.pip_theta_corr = event->pip_theta_corrected();
         // output.pip_phi_corr = event->pip_Phi_corrected();
 
-        output.pim_mom_exclusive = event->pim_momentum_measured();
-        output.pim_theta_exclusive = event->pim_momentum_corrected();
-        // output.pim_theta_exclusive = event->pim_theta_lab_measured();
+        // output.pim_mom_exclusive = event->pim_momentum_measured();
+        output.pim_mom_exclusive = event->pim_momentum_corrected();
+        output.pim_theta_exclusive = event->pim_theta_lab_measured();
         output.pim_phi_exclusive = event->pim_Phi_lab_measured();
         // output.pim_mom_corr = event->pim_momentum_corrected();
         // output.pim_theta_corr = event->pim_theta_corrected();
