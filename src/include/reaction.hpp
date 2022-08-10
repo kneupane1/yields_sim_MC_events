2c571eb72354f6fc5af2ee6d0ec0258b726aebd8
Krishna Neupane <kneupane@email.sc.edu>
2022-08-10 10:02:12 -0400

    Update reaction.hpp

--- src/include/reaction.hpp
+++ src/include/reaction.hpp
@@ -176,12 +176,11 @@ class Reaction {
   double _py_prime_prot_E = NAN;
   double _pz_prime_prot_E = NAN;
 
-  float alpha_prot_mom_corr_FD[2] = {0.2,0.5};
-  float alpha_prot_mom_corr_CD[5] = {1.0, 0.2, 0.3, 0.7, 0.9}; //sec 2,3 has 2 alpha
+  // float alpha_prot_mom_corr_FD[2] = {0.2,0.5};
+  // float alpha_prot_mom_corr_CD[5] = {1.0, 0.2, 0.3, 0.7, 0.9}; //sec 2,3 has 2 alpha
 
-  //   float alpha_prot_mom_corr_2nd[3] = {0.8, 0.0, 0.0};  // CD , FD < 27 (DEG), FD > 27 (DEG)
-
-  //   float alpha_prot_mom_corr_2nd[3] = {0.0, 0.0, 0.0};  // CD , FD < 27 (DEG), FD > 27 (DEG)
+  float alpha_prot_mom_corr_FD[2] = {0.0, 0.0};
+  float alpha_prot_mom_corr_CD[5] = {0.0, 0.0, 0.0, 0.0, 0.0};  // sec 2,3 has 2 alpha
 
   double _prot_mom_prime = NAN;
   double _prot_mom = NAN;
@@ -306,8 +305,12 @@ class Reaction {
   double _py_prime_pip_E_tmt = NAN;
   double _pz_prime_pip_E_tmt = NAN;
 
-  float alpha_pip_mom_corr_FD[2] = {0.5, 0.7};
-  float alpha_pip_mom_corr_CD[3] = {1.0, 0.25, 0.75};
+  // float alpha_pip_mom_corr_FD[2] = {0.5, 0.7};
+  // float alpha_pip_mom_corr_CD[3] = {1.0, 0.25, 0.75};
+
+  float alpha_pip_mom_corr_FD[2] = {0.0, 0.0};
+  float alpha_pip_mom_corr_CD[5] = {0.0, 0.0, 0.0};
+
   double _pip_mom = NAN;
   double _pip_mom_prime = NAN;
 
@@ -427,8 +430,11 @@ class Reaction {
   double _py_prime_pim_E_tmt = NAN;
   double _pz_prime_pim_E_tmt = NAN;
 
-  float alpha_pim_mom_corr_FD[2] = {0.5, 0.6};
-  float alpha_pim_mom_corr_CD[4] = {0.0,1.0,1.0, 1.0}; //sec 1 has 2,
+  // float alpha_pim_mom_corr_FD[2] = {0.5, 0.6};
+  // float alpha_pim_mom_corr_CD[4] = {0.0,1.0,1.0, 1.0}; //sec 1 has 2,
+
+  float alpha_pim_mom_corr_FD[2] = {0.0, 0.0};
+  float alpha_pim_mom_corr_CD[5] = {0.0, 0.0, 0.0, 0.0};
 
   // float alpha_pim_mom_corr_2nd[3] = {0.0, 0.0, 0.0};  // CD , FD < 27 (DEG), FD > 27 (DEG)
   double _pim_mom = NAN;
