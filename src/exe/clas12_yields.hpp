a7041aef280e6f78dc6390d24ba8006ace710f3e
Krishna Neupane <kneupane@email.sc.edu>
2022-10-03 13:16:16 -0400

    Update clas12_yields.hpp

--- src/exe/clas12_yields.hpp
+++ src/exe/clas12_yields.hpp
@@ -138,8 +138,8 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<SyncFile>& _syn
 
       // output.electron_sector = event->sec();
       // output.pim_sec = event->pimSec();
-      // output.pip_sec = event->pipSec();
-      output.prot_sec = event->protSec();
+      output.pip_sec = event->pipSec();
+      // output.prot_sec = event->protSec();
       // output.w = event->W();
       // output.q2 = event->Q2();
       // output.w_had = event->w_hadron();
