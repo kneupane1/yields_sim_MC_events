a14867cbe3fbbd2c40f73ddc2a28d3678c7f5dd0
Krishna Neupane <kneupane@email.sc.edu>
2023-01-20 17:39:14 -0500

    INCLUDING CD AND status

--- src/include/csv_data.hpp
+++ src/include/csv_data.hpp
@@ -20,13 +20,15 @@ struct csv_data {
   float gen_pim_theta;
   float gen_pim_phi;
 
+  int status_Pim;
+  int status_Pip;
+  int status_Prot;
 
   // Static functions can be called without making a new struct
   static std::string header() {
     // Make a string for the header of the csv file mPim case
     // return "pim_mom_mPim,pim_theta_mPim,pim_phi_mPim,mm2_mPim,mm2_mPim_corr,weight";
-    return "pim_mom_mPim,pim_theta_mPim,pim_phi_mPim,mm2_mPim,weight";
-
+    return "pim_mom_mPim,pim_theta_mPim,pim_phi_mPim,mm2_mPim,status_Pim,status_Pip,status_Prot,weight";
   }
 
   friend std ::ostream &operator<<(std::ostream &os, const csv_data &data) {
@@ -39,8 +41,13 @@ struct csv_data {
     os << std::setprecision(7);
     os << data.mm2_mPim << ",";
     //  os << data.mm2_mPim_corr << ",";
-     os << std::setprecision(7);
-     os << data.weight_mPim << ",";
+    os << std::setprecision(1);
+
+    os << data.status_Pim << ",";
+    os << data.status_Pip << ",";
+    os << data.status_Prot << ",";
+    os << std::setprecision(7);
+    os << data.weight_mPim << ",";
 
 
     return os;
