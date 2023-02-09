356fcf76cea106e0d0cafc2a6a1013898fe5c56e
Krishna Neupane <kneupane@email.sc.edu>
2023-02-09 16:17:26 -0500

    Update mom_corr.cpp

--- src/mom_corr.cpp
+++ src/mom_corr.cpp
@@ -574,7 +574,7 @@ double dppC(float Px, float Py, float Pz, int sec, int ivec) {
 
 // our hadron momentum correction come from here:
 // proton mom corr
-float alpha_prot_mom_corr_FD[5] = {0.7,0.5, 0.6, 0.6}; // all fd low, except all fd low, all fd high, except all fd high
+float alpha_prot_mom_corr_FD[5] = {0.7,0.5, 0.6, 0.9}; // all fd low, except all fd low, all fd high, except all fd high
 float alpha_prot_mom_corr_CD[3] = {0.9, 0.5, 0.95};
 
 // float alpha_prot_mom_corr_FD[2] = {1., 1.};
