a3bad3ce97445d49f7e8bf654cd2ab4647376d12
Krishna Neupane <kneupane@email.sc.edu>
2023-04-14 19:54:52 -0400

    now for 10% variation on my best alpha values

--- src/exe/clas12_yields.hpp
+++ src/exe/clas12_yields.hpp
@@ -14,11 +14,11 @@
 #include "syncfile.hpp"
 using namespace std;
 
-// // float alpha_CD[3][3] = {{0.9, 0.9, 0.95}, {0.8, 0.4, 0.8}, {0.5, 1.0, 0.5}};
-// // float alpha_FD[3][4] = {{0.5, 0.6, 0.5, 0.5}, {0.1, 0.15, 0.5, 0.5}, {0.5, 0.15, 0.3, 0.3}};
+float alpha_CD[3][3] = {{0.9, 0.9, 0.95}, {0.8, 0.4, 0.8}, {0.5, 1.0, 0.5}};
+float alpha_FD[3][4] = {{0.5, 0.6, 0.5, 0.5}, {0.1, 0.15, 0.5, 0.5}, {0.5, 0.15, 0.3, 0.3}};
 
-float alpha_CD[3][3];
-float alpha_FD[3][4];
+// float alpha_CD[3][3];
+// float alpha_FD[3][4];
 
 void initialize_alphas() {
   std::srand(std::chrono::duration_cast<std::chrono::milliseconds>(
@@ -28,8 +28,8 @@ void initialize_alphas() {
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 4; j++) {
       float rand_no = static_cast<float>(std::rand()) / RAND_MAX;
-      // alpha_FD[i][j] += alpha_FD[i][j] * (rand_no - 0.5) * 0.1;
-      alpha_FD[i][j] = rand_no;
+      alpha_FD[i][j] += alpha_FD[i][j] * (rand_no - 0.5) * 0.1;
+      // alpha_FD[i][j] = rand_no;
 
       std::cout << "fd rand no: " << rand_no << "  alpha : " << alpha_FD[i][j] << std::endl;
     }
@@ -38,8 +38,8 @@ void initialize_alphas() {
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       float rand_no = static_cast<float>(std::rand()) / RAND_MAX;
-      // alpha_CD[i][j] += alpha_CD[i][j] * (rand_no - 0.5) * 0.1;
-      alpha_CD[i][j] = rand_no;
+      alpha_CD[i][j] += alpha_CD[i][j] * (rand_no - 0.5) * 0.1;
+      // alpha_CD[i][j] = rand_no;
 
       std::cout << " cd rand no: " << rand_no << "  alpha : " << alpha_CD[i][j] << std::endl;
     }
