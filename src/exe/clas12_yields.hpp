55806c8bf8546e7801ecf53c5989b590a2290802
Krishna Neupane <kneupane@email.sc.edu>
2023-07-06 07:12:27 -0400

    Update clas12_yields.hpp

--- src/exe/clas12_yields.hpp
+++ src/exe/clas12_yields.hpp
@@ -120,9 +120,9 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<SyncFile>& _syn
     // if (event->TwoPion_missingPim()) {
     // if (event->TwoPion_missingPip()) {
     // if (event->TwoPion_missingProt()) {
-    // if (event->TwoPion_exclusive()) {
+    if (event->TwoPion_exclusive()) {
 
-    if (event->Inclusive()) {
+    // if (event->Inclusive()) {
       {
         // if (event->W() > 1.25 && event->W() < 2.55 && event->Q2() > 1.5 && event->Q2() < 10.5){
         // &&
