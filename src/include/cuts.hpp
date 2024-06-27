104f176fe90909cf6b2248ccee5ed1f003a7146b
Krishna Neupane <kneupane@email.sc.edu>
2024-06-27 13:14:33 -0400

    updated pass2 cuts

--- src/include/cuts.hpp
+++ src/include/cuts.hpp
@@ -7,57 +7,67 @@
 #include "physics.hpp"
 
 class Cuts {
-protected:
-std::shared_ptr<Branches12> _data = nullptr;
-std::shared_ptr<Delta_T> _dt = nullptr;
+ protected:
+  std::shared_ptr<Branches12> _data = nullptr;
+  std::shared_ptr<Delta_T> _dt = nullptr;
 
-public:
-Cuts(const std::shared_ptr<Branches12>& data);
-Cuts(const std::shared_ptr<Branches12>& data, const std::shared_ptr<Delta_T>& dt);
-~Cuts();
+ public:
+  Cuts(const std::shared_ptr<Branches12> &data);
+  Cuts(const std::shared_ptr<Branches12> &data, const std::shared_ptr<Delta_T> &dt);
+  ~Cuts();
 
-bool ElectronCuts();
-bool FiducialCuts();
-bool IsPip(int i);
-bool IsProton(int i);
-bool IsPim(int i);
+  bool ElectronCuts();
+  bool FiducialCuts();
+  bool IsPip(int i);
+  bool IsProton(int i);
+  bool IsPim(int i);
 };
 
 class rga_Cuts : public Cuts {
-public:
-rga_Cuts(const std::shared_ptr<Branches12>& data) : Cuts(data) {
-}
-rga_Cuts(const std::shared_ptr<Branches12>& data, const std::shared_ptr<Delta_T>& dt) : Cuts(data, dt){
-};
+ public:
+  rga_Cuts(const std::shared_ptr<Branches12> &data) : Cuts(data) {}
+  rga_Cuts(const std::shared_ptr<Branches12> &data, const std::shared_ptr<Delta_T> &dt) : Cuts(data, dt){};
 };
 
 class rgf_Cuts : public Cuts {
-public:
-rgf_Cuts(const std::shared_ptr<Branches12>& data) : Cuts(data) {
-}
-rgf_Cuts(const std::shared_ptr<Branches12>& data, const std::shared_ptr<Delta_T>& dt) : Cuts(data, dt){
-};
+ public:
+  rgf_Cuts(const std::shared_ptr<Branches12> &data) : Cuts(data) {}
+  rgf_Cuts(const std::shared_ptr<Branches12> &data, const std::shared_ptr<Delta_T> &dt) : Cuts(data, dt){};
 };
 
 class uconn_Cuts : public Cuts {
-public:
-uconn_Cuts(const std::shared_ptr<Branches12>& data) : Cuts(data) {
-}
-uconn_Cuts(const std::shared_ptr<Branches12>& data, const std::shared_ptr<Delta_T>& dt) : Cuts(data, dt){
-};
-bool ElectronCuts();
+ public:
+  uconn_Cuts(const std::shared_ptr<Branches12> &data) : Cuts(data) {}
+  uconn_Cuts(const std::shared_ptr<Branches12> &data, const std::shared_ptr<Delta_T> &dt) : Cuts(data, dt){};
+  bool ElectronCuts();
 
-// bool CC_nphe_cut(double nphe);
-bool CC_nphe_cut();
-bool EC_outer_vs_EC_inner_cut();
-bool EC_sampling_fraction_cut();
-bool EC_hit_position_fiducial_cut_homogeneous();
-bool DC_fiducial_cut_XY();
-bool DC_z_vertex_cut();
+  // bool CC_nphe_cut(double nphe);
+  bool CC_nphe_cut();
+  bool PCAL_minimum_energy();
+  bool EC_sampling_fraction_cut();
+  bool EC_hit_position_fiducial_cut_homogeneous();
+  bool DC_fiducial_cut_XY(int i);
+  bool DC_z_vertex_cut();
+  bool EC_inner_vs_EC_outer();
+  bool PCAL_fiducial_cut_HX_HY();
 
-bool HadronsCuts(int i);
-bool DC_fiducial_cut_theta_phi(int i);
-bool Hadron_Delta_vz_cut(int i);
-bool Hadron_Chi2pid_cut(int i);
+  bool HadronsCuts(int i);
+  bool DC_fiducial_cut_theta_phi(int i);
+  bool Hadron_Delta_vz_cut(int i);
+  bool Hadron_Chi2pid_cut(int i);
+  bool CD_fiducial_had(int i);
+  // Function to get the momentum range index based on the value of p
+
+  int getMomRangeIndex(double p) {
+    const double boundaries[] = {2, 3, 4, 5, 6, 7, 8, 9};
+    const int numBoundaries = sizeof(boundaries) / sizeof(boundaries[0]);
+
+    for (int i = 0; i < numBoundaries; ++i) {
+      if (p < boundaries[i]) {
+        return i;
+      }
+    }
+    return numBoundaries;  // For p >= 9
+  }
 };
 #endif
